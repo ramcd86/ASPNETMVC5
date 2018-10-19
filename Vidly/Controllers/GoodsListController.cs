2a23584c803235718a8c0e164088f6c944cf0df9
Ross McDermott <ross.mcdermott@madetoengage.com>
2018-10-19 09:40:04 +0100

    Ongoing work.

---
+++ Vidly/Controllers/GoodsListController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class GoodsListController : Controller
+    {
+        // GET: GoodsList
+        [Route("GoodsList/List")]
+        // The View MUST be the same as the function name.
+        public ActionResult GoodsList()
+        {
+            return View();
+        }
+    }
+}
